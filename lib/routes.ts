33360977e6029d0cea27f1794e45a5d7eb393b89
Ampaabeng Kyeremeh Nancy <kampaabeng01@gmail.com>
2026-02-07 23:39:27 +0000

    style: fix formatting across 36 files to resolve CI error

--- lib/routes.ts
+++ lib/routes.ts
@@ -9,7 +9,8 @@ export const ROUTES = {
 
   social: {
     instagram: 'https://www.instagram.com/p/DTks9jUjSqj/?igsh=MXJmbWZ6Y3ZpY3VlbA==',
-    linkedin: 'https://www.linkedin.com/posts/coltek-technologies_techtips-coltektechnologies-digitalsecurity-activity-7417922267709390850-ht9L?utm_source=share&utm_medium=member_ios&rcm=ACoAAEzJEWcBxpz4Lo2k3w7j0HRJvJ7whWOMr9Q',
+    linkedin:
+      'https://www.linkedin.com/posts/coltek-technologies_techtips-coltektechnologies-digitalsecurity-activity-7417922267709390850-ht9L?utm_source=share&utm_medium=member_ios&rcm=ACoAAEzJEWcBxpz4Lo2k3w7j0HRJvJ7whWOMr9Q',
     x: 'https://x.com/coltekdev/status/2012157442575290678?s=46',
     facebook: 'https://www.facebook.com/share/p/1CtM43AXru/?mibextid=wwXIfr',
   },
