3c1bd924a6151de75f3b225b0038c0320dbe88cf
oldcode-dev <ernestfrimpongopoku.992@outlook.com>
2026-04-06 11:39:33 +0000

    FIX: removed all unused imports and files

--- app/departments/communications/page.tsx
+++ app/departments/communications/page.tsx
@@ -1,4 +1,3 @@
-import { MessageSquare } from 'lucide-react';
 import { BulkMessagingDashboard } from '@/components/departments/MessageMembers/BulkMessagingDashboard';
 
 export default function CommunicationsPage() {
