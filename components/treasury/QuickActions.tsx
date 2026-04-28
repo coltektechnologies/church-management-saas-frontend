7c0eedbe020dbc8e07a96f1b0e81a64bba44b241
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2026-04-28 18:24:19 +0000

    Update QuickActions.tsx

--- components/treasury/QuickActions.tsx
+++ components/treasury/QuickActions.tsx
@@ -13,7 +13,6 @@ import {
   type LucideIcon,
 } from 'lucide-react';
 import Image from 'next/image';
-
 interface QuickActionsProps {
   onRecordIncome?: () => void;
   onRecordExpenditure?: () => void;
